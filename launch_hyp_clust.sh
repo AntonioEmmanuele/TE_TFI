7918321968469ba1f0fd25cce50221b71a04727b
Antonio <antony.35.ae@gmail.com>
2024-12-06 22:14:53 +0100

    adds CLUST Series: ./datasets/processed/001_UCR_Anomaly_DISTORTED1sddb40_35000_52000_52620.txt N_clust :  Perc :

--- launch_hyp_clust.sh
+++ launch_hyp_clust.sh
@@ -4,7 +4,7 @@ out_dir="hyp_test_ucr_new"
 for file in ./datasets/processed/*; do
     python3 ./hyp_cluster.py --cluster_min 2 --cluster_max 10 --path_stagionality "./tst/ucr_seasonality_results_0.6.csv" --win_clust $win_clust --series_path $file --out_path $out_dir
     git add *
-    git commit -m "adds Series: $file N_clust : $n_clust Perc : $perc_clust"
+    git commit -m "adds CLUST Series: $file N_clust : $n_clust Perc : $perc_clust"
     git push
 done
 
