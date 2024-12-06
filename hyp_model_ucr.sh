241d45d8e3114ca0126503400a7382515363a38b
antonio <antony.35.ae@gmail.com>
2024-12-06 16:59:07 +0100

    adds rt

--- hyp_model_ucr.sh
+++ hyp_model_ucr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 win_clust=50
-out_dir="hyp_rt"
+out_dir="hyp_rt_ucr"
 for file in ./datasets/processed/*; do
-    python3 ./hyp_cluster.py --path_stagionality "./tst/ucr_seasonality_results_0.6.csv" --num_cluster $n_clust --win_size $win_clust --series_path $file  --lag_percentage 1.0 --out_path $out_dir
+    python3 ./hyp_models.py --model "RT" --path_stagionality "./tst/ucr_seasonality_results_0.6.csv" --win_size $win_clust --series_path $file  --lag_percentage 1.0 --out_path $out_dir
 done
 
