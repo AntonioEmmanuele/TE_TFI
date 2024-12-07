5e4bdd2b8e1ccf4bda06912cc5bbcd053a79e608
antonio <antony.35.ae@gmail.com>
2024-12-07 11:04:37 +0100

    adds Random Forest ./datasets/processed/250_UCR_Anomaly_weallwalk_2951_7290_7296.txt

--- hyp_model_ucr_randomfor.sh
+++ hyp_model_ucr_randomfor.sh
@@ -15,6 +15,10 @@ done
 # Iterate over the range from start_index to end_index
 for ((i=offset; i<end_offset; i++)); do
   echo "Processing: ${files[i]}"
+  python3 ./hyp_models.py --model "RF" --path_stagionality "./tst/ucr_seasonality_results_0.6.csv" --win_size $win_clust --series_path $file  --lag_percentage 1.0 --out_path $out_dir
+  git add *
+  git commit -m "adds Random Forest ${file}"
+  git push
 done
 # for file in "${files[@]:$offset:}"; do
 #   echo "Processing: $file"
