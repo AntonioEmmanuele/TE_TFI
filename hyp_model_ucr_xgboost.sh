de2afbb90588667295f96b223e3fcc92a40d93a3
antonio <antony.35.ae@gmail.com>
2024-12-07 19:47:52 +0100

    adds XGBoost ./datasets/processed/001_UCR_Anomaly_DISTORTED1sddb40_35000_52000_52620.txt

--- hyp_model_ucr_xgboost.sh
+++ hyp_model_ucr_xgboost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 win_clust=50
-out_dir="hyp_xgboost_ucr"
+out_dir="hyp_xgboost_ucr_2"
 files=()
-offset=100 # Used for experiments error
-end_offset=250 # Used for splitting experiments
+offset=0 # Used for experiments error
+end_offset=100 # Used for splitting experiments
 # Add each file in the directory to the array
 for file in ./datasets/processed/*; do 
   # Check if it's a regular file
@@ -15,7 +15,7 @@ done
 # Iterate over the range from start_index to end_index
 for ((i=offset; i<end_offset; i++)); do
   	echo "Processing: ${files[i]}"
-	python3 ./hyp_models.py --model "RF" --path_stagionality "./tst/ucr_seasonality_results_0.6.csv" --win_size $win_clust --series_path ${files[i]}  --lag_percentage 1.0 --out_path $out_dir
+	python3 ./hyp_models.py --model "XGB" --path_stagionality "./tst/ucr_seasonality_results_0.6.csv" --win_size $win_clust --series_path ${files[i]}  --lag_percentage 1.0 --out_path $out_dir
 	git add *
 	git commit -m "adds XGBoost ${files[i]}"
 	git push
